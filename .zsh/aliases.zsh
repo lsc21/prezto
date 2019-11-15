bd3b6d6d1889b15adc0cc7578bc94258dde21af7
Louis Clotman <louis.clotman@aixplatform.com>
2019-11-14 23:24:22 -0500

    enhancements

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,6 +1,9 @@
 # A L I A S E S
 alias     less="less -R"
-alias     travis='RAILS_ENV=test DO_NOT_ADD_FIPS=true bundle exec rake --trace travis:test'
+#alias     travis='RAILS_ENV=test DO_NOT_ADD_FIPS=true bundle exec rake --trace travis:test'
+alias     git="hub"
+alias     t="travis"
+alias     epoch='date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s"'
 
 # Download and Extract Archives with WGET
 alias     wxz='f() { wget $1 -O - | tar -xz};f'
@@ -11,3 +14,12 @@ alias     wx='f() { wget $1 -O - | tar -x};f'
 alias     kbDevicted="kbg po -a | grep Evicted | awk '{print $1}' | xargs kubectl delete pod"
 alias     gb="git branch | fzf-tmux -d 15"
 alias     gdns="gcloud dns record-sets list -z feature"
+
+# Debugging
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+
+# AIX DevOps
+alias kaixstaging='kubectl --kubeconfig=/Users/lclotman/Code/infrastructure-staging/kubeconfig'
+alias haixstaging='KUBECONFIG=/Users/lclotman/Code/infrastructure-staging/kubeconfig HELM_HOME=/Users/lclotman/helm helm'
+alias haixprod='KUBECONFIG=/Users/lclotman/Code/infrastructure-production/kubeconfig HELM_HOME=/Users/lclotman/.helm helm'
+alias haixuat='KUBECONFIG=/Users/lclotman/Code/infrastructure-uat/kubeconfig HELM_HOME=/Users/lclotman/.helm helm'
--- .zshrc
+++ .zshrc
@@ -2,22 +2,24 @@
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
+[ -f /Users/lclotman/.travis/travis.sh ] && source /Users/lclotman/.travis/travis.sh
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-source "$HOME/.zsh/git-flow-completion.zsh"
+export PATH=$HOME/.rbenv/versions/2.6.5/bin:/usr/local/bin:/usr/local/opt:$PATH
+
 source "$HOME/.zsh/aliases.zsh"     # Load Aliases
 source "$HOME/.zsh/options.zsh"     # Load options
 source "$HOME/.zsh/bindkeys.zsh"    # Load bindkeys
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
 # load custom executable functions
 for function in ~/.zsh/functions/*; do
   source $function
 done
 
-# R B E N V
 eval "$(rbenv init -)"
-
-# S S H  A G E N T
 eval `ssh-agent -s`
+eval "$(direnv hook zsh)"
 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
@@ -29,3 +31,6 @@ if [ -f '/Users/louis/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/loui
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/louis/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/louis/google-cloud-sdk/completion.zsh.inc'; fi
+
+# attach to tmux session if it exists; if not, create it
+tmux new-session -A -s main
