f6268de623224b7fab15c287524a7fe8b1c75b29
Louis Clotman <louis.clotman@aixplatform.com>
2020-02-28 15:13:59 -0500

    clean up zsh options

--- .zsh/options.zsh
+++ .zsh/options.zsh
@@ -1,30 +1,13 @@
-# why would you type 'cd dir' if you could just type 'dir'?
-setopt AUTO_CD
-
-# This makes cd=pushd
-setopt AUTO_PUSHD
-
-# If we have a glob this will expand it
-setopt GLOB_COMPLETE
-setopt PUSHD_MINUS
-
-# this will ignore multiple directories for the stack.  Useful?  I dunno.
-setopt PUSHD_IGNORE_DUPS
-
-# If I could disable Ctrl-s completely I would!
-setopt NO_FLOW_CONTROL
-
-# Keep echo "station" > station from clobbering station
-setopt NO_CLOBBER
-
-# Case insensitive globbing
-setopt NO_CASE_GLOB
-
-# Be Reasonable!
-setopt NUMERIC_GLOB_SORT
-
-# I don't know why I never set this before.
-setopt EXTENDED_GLOB
-
-# Awesome arrays, (that is, frew${cool}frew has frew surrounding all the variables, not just first and last
-setopt RC_EXPAND_PARAM
+setopt AUTO_CD                    # why would you type 'cd dir' if you could just type 'dir'?
+setopt AUTO_PUSHD                 # This makes cd=pushd
+setopt GLOB_COMPLETE              # Cycle through glob matches
+setopt PUSHD_MINUS                # Swaps the meanings of '+' and '-' when numerically specifying stack directories
+setopt PUSHD_IGNORE_DUPS          # Don't push multiple copies of the same directory onto the directory stack.
+setopt NO_FLOW_CONTROL            # If I could disable Ctrl-s completely I would!
+setopt CLOBBER                    # Allows '>' redirection to truncate existing files, and '>>' to create files. 
+setopt NO_CASE_GLOB               # Case insensitive globbing
+setopt NUMERIC_GLOB_SORT          # Sort numeric filenames numerically rather than lexicographically.
+setopt EXTENDED_GLOB              # Treat the '#', '~' and '^' characters as part of patterns for filename generation
+setopt RC_EXPAND_PARAM            # Read the docs about array expansion to understand this.
+setopt INTERACTIVE_COMMENTS       # Allow comments even in interactive shells.
+setopt VI                         # Has the equivalent effect of 'bindkey -v'.
